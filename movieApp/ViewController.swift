a910b567e0fbf7bb2d9653ad6360519a2b38ffec
sfctl <catalbasifa@gmail.com>
2020-10-21 19:24:37 +0300

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Yasemin YEL on 21.10.2020.
+//  Copyright © 2020 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
